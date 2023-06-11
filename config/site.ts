3f4575cef87bad382d9bcfce09ea3066a5a2b18b
Thigo <thi.it.solutions@gmail.com>
2023-06-11 04:24:41 -0300

    chore(): initial commit create project

---
+++ config/site.ts
@@ -0,0 +1,14 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "Store 33",
+  description: "Online store of clothes, shoes, bags and accessories.",
+  footer: [
+    { name: "Home", href: "/" },
+    { name: "About", href: "#" },
+    { name: "Terms & Conditions", href: "#" },
+    { name: "Shipping & Return Policy", href: "#" },
+    { name: "Privacy Policy", href: "#" },
+    { name: "FAQ", href: "#" },
+  ],
+}
