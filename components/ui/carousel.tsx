6d4e04bcfa6e40ec0d5b3fc0f319e17f6c2b221f
Thigo <thi.it.solutions@gmail.com>
2023-06-25 00:09:26 -0300

    feat(LCP): adding Next Image in carousel images

--- components/ui/carousel.tsx
+++ components/ui/carousel.tsx
@@ -46,6 +46,8 @@ export function ImageCarousel() {
           <Image
             className="rounded-lg border"
             src="https://cdn.sanity.io/images/m8ei9ib0/production/ab4ab219aed08c966943581a6eba72d83c823aa8-680x850.jpg?w=2000&fit=max&auto=format"
+            width={400}
+            height={400}
             alt=""
           />
         </Carousel.Slide>
@@ -53,6 +55,8 @@ export function ImageCarousel() {
           <Image
             className="rounded-lg border"
             src="https://cdn.sanity.io/images/m8ei9ib0/production/5a073e0baef2c640b6bc8a77f837e034f1eb9059-680x850.jpg?w=2000&fit=max&auto=format"
+            width={400}
+            height={400}
             alt=""
           />
         </Carousel.Slide>
@@ -60,6 +64,8 @@ export function ImageCarousel() {
           <Image
             className="rounded-lg border"
             src="https://cdn.sanity.io/images/m8ei9ib0/production/b27e36756e9ae90c5ebc303ed12741d002fea7a4-680x850.jpg?w=2000&fit=max&auto=format"
+            width={400}
+            height={400}
             alt=""
           />
         </Carousel.Slide>
@@ -67,6 +73,8 @@ export function ImageCarousel() {
           <Image
             className="rounded-lg border"
             src="https://cdn.sanity.io/images/m8ei9ib0/production/4f86ba8ba779776820152ab5b2049ca47d8f7b21-680x850.jpg?w=2000&fit=max&auto=format"
+            width={400}
+            height={400}
             alt=""
           />
         </Carousel.Slide>
@@ -74,6 +82,8 @@ export function ImageCarousel() {
           <Image
             className="rounded-lg border"
             src="https://cdn.sanity.io/images/m8ei9ib0/production/c7e64679edcdcc21b2a57cdef75792714ae94b59-680x850.jpg?w=2000&fit=max&auto=format"
+            width={400}
+            height={400}
             alt=""
           />
         </Carousel.Slide>
