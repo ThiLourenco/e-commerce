9e95c348f5361fbafac6727f2d4a3e9a0dd76c03
Thigo <thi.it.solutions@gmail.com>
2023-06-25 00:41:34 -0300

    feat(LCP): remove Next Image bug

--- components/ui/carousel.tsx
+++ components/ui/carousel.tsx
@@ -4,7 +4,6 @@ import { useRef } from 'react'
 import { Carousel } from '@mantine/carousel'
 import { createStyles, getStylesRef } from '@mantine/core'
 import Autoplay from 'embla-carousel-autoplay'
-import Image from 'next/image'
 
 export function ImageCarousel() {
   const autoplay = useRef(Autoplay({ delay: 2000 }))
@@ -43,47 +42,34 @@ export function ImageCarousel() {
         onMouseLeave={autoplay.current.reset}
       >
         <Carousel.Slide size={400}>
-          <Image
+          <img
             className="rounded-lg border"
             src="https://cdn.sanity.io/images/m8ei9ib0/production/ab4ab219aed08c966943581a6eba72d83c823aa8-680x850.jpg?w=2000&fit=max&auto=format"
-            width={400}
-            height={400}
-            alt=""
           />
         </Carousel.Slide>
         <Carousel.Slide size={400}>
-          <Image
+          <img
             className="rounded-lg border"
             src="https://cdn.sanity.io/images/m8ei9ib0/production/5a073e0baef2c640b6bc8a77f837e034f1eb9059-680x850.jpg?w=2000&fit=max&auto=format"
-            width={400}
-            height={400}
-            alt=""
           />
         </Carousel.Slide>
         <Carousel.Slide size={400}>
-          <Image
+          <img
             className="rounded-lg border"
             src="https://cdn.sanity.io/images/m8ei9ib0/production/b27e36756e9ae90c5ebc303ed12741d002fea7a4-680x850.jpg?w=2000&fit=max&auto=format"
-            width={400}
-            height={400}
-            alt=""
           />
         </Carousel.Slide>
         <Carousel.Slide size={400}>
-          <Image
+          <img
             className="rounded-lg border"
             src="https://cdn.sanity.io/images/m8ei9ib0/production/4f86ba8ba779776820152ab5b2049ca47d8f7b21-680x850.jpg?w=2000&fit=max&auto=format"
-            width={400}
-            height={400}
             alt=""
           />
         </Carousel.Slide>
         <Carousel.Slide size={400}>
-          <Image
+          <img
             className="rounded-lg border"
             src="https://cdn.sanity.io/images/m8ei9ib0/production/c7e64679edcdcc21b2a57cdef75792714ae94b59-680x850.jpg?w=2000&fit=max&auto=format"
-            width={400}
-            height={400}
             alt=""
           />
         </Carousel.Slide>
