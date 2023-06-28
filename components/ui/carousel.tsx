e5df3574bd61bb5e022fd14e2222b259c317b94b
Thigo <thi.it.solutions@gmail.com>
2023-06-27 23:28:27 -0300

    fix(image):removed Next Image; changed src image

--- components/ui/carousel.tsx
+++ components/ui/carousel.tsx
@@ -44,32 +44,32 @@ export function ImageCarousel() {
         <Carousel.Slide size={400}>
           <img
             className="rounded-lg border"
-            src="https://cdn.sanity.io/images/m8ei9ib0/production/ab4ab219aed08c966943581a6eba72d83c823aa8-680x850.jpg?w=2000&fit=max&auto=format"
+            src="https://cdn.sanity.io/images/d2tfklso/production/ab4ab219aed08c966943581a6eba72d83c823aa8-680x850.jpg?w=2000&fit=max&auto=format"
           />
         </Carousel.Slide>
         <Carousel.Slide size={400}>
           <img
             className="rounded-lg border"
-            src="https://cdn.sanity.io/images/m8ei9ib0/production/5a073e0baef2c640b6bc8a77f837e034f1eb9059-680x850.jpg?w=2000&fit=max&auto=format"
+            src="https://cdn.sanity.io/images/d2tfklso/production/5a073e0baef2c640b6bc8a77f837e034f1eb9059-680x850.jpg?w=2000&fit=max&auto=format"
           />
         </Carousel.Slide>
         <Carousel.Slide size={400}>
           <img
             className="rounded-lg border"
-            src="https://cdn.sanity.io/images/m8ei9ib0/production/b27e36756e9ae90c5ebc303ed12741d002fea7a4-680x850.jpg?w=2000&fit=max&auto=format"
+            src="https://cdn.sanity.io/images/d2tfklso/production/c7e64679edcdcc21b2a57cdef75792714ae94b59-680x850.jpg?w=2000&fit=max&auto=format"
           />
         </Carousel.Slide>
         <Carousel.Slide size={400}>
           <img
             className="rounded-lg border"
-            src="https://cdn.sanity.io/images/m8ei9ib0/production/4f86ba8ba779776820152ab5b2049ca47d8f7b21-680x850.jpg?w=2000&fit=max&auto=format"
+            src="https://cdn.sanity.io/images/d2tfklso/production/b27e36756e9ae90c5ebc303ed12741d002fea7a4-680x850.jpg?w=2000&fit=max&auto=format"
             alt=""
           />
         </Carousel.Slide>
         <Carousel.Slide size={400}>
           <img
             className="rounded-lg border"
-            src="https://cdn.sanity.io/images/m8ei9ib0/production/c7e64679edcdcc21b2a57cdef75792714ae94b59-680x850.jpg?w=2000&fit=max&auto=format"
+            src="https://cdn.sanity.io/images/d2tfklso/production/88b9639b32832d4d354a9067826ca08456ba0141-680x850.jpg?w=2000&fit=max&auto=format"
             alt=""
           />
         </Carousel.Slide>
