b42e1df91178e63b55363f291e60efafc1eaa6b2
Thigo <thi.it.solutions@gmail.com>
2023-07-15 22:18:00 -0300

    feat(): creating orders details using table

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { OrderItem } from '@/components/order'
-import { OrderSummary } from '@/components/orderSummary'
 import { useUser } from '@clerk/nextjs'
 import { useRouter } from 'next/navigation'
 
@@ -16,21 +15,19 @@ const Dashboard = () => {
   return (
     <section className="grid min-h-full place-items-center px-6 py-24 sm:py-32 lg:px-8">
       <div>
-        <main className="mx-auto max-w-2xl px-4 pb-24 pt-16 sm:px-6 lg:max-w-7xl lg:px-8">
+        <main className="mx-auto max-w-2xl items-center px-4 pb-24 pt-4 sm:px-6 lg:max-w-7xl lg:px-8">
           <h1 className="text-3xl font-bold tracking-tight sm:text-4xl">
             Detalhes do pedido
           </h1>
 
           <div className="mt-12 lg:grid lg:grid-cols-12 lg:items-start lg:gap-x-12 xl:gap-x-16">
-            <section aria-labelledby="cart-heading" className="lg:col-span-7">
+            <section className="lg:col-span-12">
               <h2 id="orders-heading" className="sr-only">
                 Verifique o status de pedidos recentes e antigos e descubra mais
                 produtos
               </h2>
-
               <OrderItem />
             </section>
-            <OrderSummary />
           </div>
         </main>
       </div>
--- components/order.tsx
+++ components/order.tsx
@@ -3,16 +3,78 @@
 export function OrderItem() {
   return (
     <>
-      <div
-        aria-labelledby="summary-heading"
-        className="mb-4 mt-16 rounded-lg border-2 border-gray-200 bg-gray-50 px-4 py-6 shadow-md dark:border-gray-900 dark:bg-black sm:p-6 lg:col-span-5 lg:mt-0 lg:p-8"
-      >
-        <h2 id="summary-heading" className="text-lg font-medium">
-          <p>ID do Pedido: #46199271460087</p>
-          <p>Data: 01 de julho de 2023</p>
-          <p>Valor Total: R$ 420,00</p>
-          <p>Status do pedido: Pendente</p>
-        </h2>
+      <div className="bg-gray-0 relative mb-4 mt-16 overflow-x-auto rounded-lg border-2 border-gray-700 px-4 py-6 shadow-md dark:border-gray-900 dark:bg-black sm:rounded-lg sm:p-6 lg:col-span-5 lg:mt-0 lg:p-8">
+        <table className="w-full text-left text-sm text-gray-500 dark:text-gray-400">
+          <thead className="ext-xs bg-gray-200  text-gray-700 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                N° Pedido
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Data
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Valor
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Rastreamento
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Status
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className="border-b bg-white hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-600">
+              <th
+                scope="row"
+                className="whitespace-nowrap px-6 py-4 font-medium text-gray-900 dark:text-white"
+              >
+                #4619
+              </th>
+              <td className="whitespace-nowrap px-6 py-4">
+                01 de Julho de 2023
+              </td>
+              <td className="whitespace-nowrap px-6 py-4">R$ 420,00 </td>
+              <td className="whitespace-nowrap px-6 py-4">
+                <a
+                  href="https://rastreamento.correios.com.br/app/index.php"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Link
+                </a>
+              </td>
+              <td className="whitespace-nowrap px-6 py-4 text-yellow-400">
+                Pendente
+              </td>
+            </tr>
+            <tr className="border-b bg-white hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-600">
+              <th
+                scope="row"
+                className="whitespace-nowrap px-6 py-4 font-medium text-gray-900 dark:text-white"
+              >
+                #4196
+              </th>
+              <td className="whitespace-nowrap px-6 py-4">
+                02 de Julho de 2023
+              </td>
+              <td className="whitespace-nowrap px-6 py-4">R$ 520,00 </td>
+              <td className="whitespace-nowrap px-6 py-4">
+                <a
+                  href="https://rastreamento.correios.com.br/app/index.php"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Link
+                </a>
+              </td>
+              <td className="whitespace-nowrap px-6 py-4 text-green-300">
+                Entregue
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </>
   )
