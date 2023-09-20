1afacf35305b5736d532446aaab1682837767d22
Thigo <thi.it.solutions@gmail.com>
2023-09-19 22:17:59 -0300

    refactor:() create pages with content loren ipsum

--- app/faq/page.tsx
+++ app/faq/page.tsx
@@ -1,7 +1,55 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@/components/ui/accordion'
+
 export default async function Page() {
   return (
-    <main className="grid place-items-center px-6 xs:py-24 sm:py-32 lg:px-8">
-      <div className="text-center">Hello World - faq</div>
+    <main className="flex-1 px-6 xs:py-24 sm:py-32 lg:px-8 ">
+      <h1 className="mx-auto max-w-6xl text-center text-4xl font-extrabold -tracking-tight">
+        Perguntas Mais Frequentes
+      </h1>
+      <div className="mx-auto max-w-6xl px-4 pt-20 text-justify">
+        <Accordion type="single" collapsible>
+          <AccordionItem value="item-1">
+            <AccordionTrigger value="item-1">Pergunta 1</AccordionTrigger>
+            <AccordionContent>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
+              pretium fringilla justo nec imperdiet. Duis at convallis lorem.
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-2">
+            <AccordionTrigger value="item-2">Pergunta 2</AccordionTrigger>
+            <AccordionContent>
+              Aenean nisl nulla, semper nec risus et, molestie efficitur eros.
+              Sed non tortor et magna mattis ullamcorper lacinia nec nisi.
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-3">
+            <AccordionTrigger value="item-3">Pergunta 3</AccordionTrigger>
+            <AccordionContent>
+              Aenean nisl nulla, semper nec risus et, molestie efficitur eros.
+              Sed non tortor et magna mattis ullamcorper lacinia nec nisi.
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-4">
+            <AccordionTrigger value="item-4">Pergunta 4</AccordionTrigger>
+            <AccordionContent>
+              Aenean nisl nulla, semper nec risus et, molestie efficitur eros.
+              Sed non tortor et magna mattis ullamcorper lacinia nec nisi.
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-5">
+            <AccordionTrigger value="item-5">Pergunta 5</AccordionTrigger>
+            <AccordionContent>
+              Aenean nisl nulla, semper nec risus et, molestie efficitur eros.
+              Sed non tortor et magna mattis ullamcorper lacinia nec nisi.
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+      </div>
     </main>
   )
 }
--- app/terms/page.tsx
+++ app/terms/page.tsx
@@ -1,7 +1,30 @@
 export default async function Page() {
   return (
     <main className="grid place-items-center px-6 xs:py-24 sm:py-32 lg:px-8">
-      <div className="text-center">Hello World - terms</div>
+      <h1 className="text-4xl font-extrabold -tracking-tight">
+        Termos e Condições
+      </h1>
+      <div className="mx-auto max-w-6xl justify-normal px-4 pt-20 text-justify">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
+        pretium fringilla justo nec imperdiet. Duis at convallis lorem. Aenean
+        nisl nulla, semper nec risus et, molestie efficitur eros. Sed non tortor
+        et magna mattis ullamcorper lacinia nec nisi. Suspendisse potenti. Morbi
+        at fringilla sem, in pellentesque arcu. Suspendisse feugiat enim nisi,
+        eget scelerisque tellus pulvinar pellentesque. Sed eu est lacinia,
+        aliquet urna ac, rhoncus nisi. Quisque magna mauris, commodo in iaculis
+        ac, ullamcorper id justo. Nulla aliquam, metus in sodales scelerisque,
+        risus orci convallis sem, vel commodo risus purus non ex. Curabitur
+        placerat neque vel nibh sagittis, quis elementum mauris consequat.
+        Nullam metus est, gravida id viverra non, sollicitudin ut odio. Aenean
+        at dictum purus. Praesent eleifend eros et nibh lobortis bibendum.
+        Curabitur varius varius ipsum vestibulum pretium. Praesent vehicula
+        felis et eros condimentum interdum. Sed laoreet turpis luctus tellus
+        vehicula, a convallis justo rutrum. Cras imperdiet massa ac turpis
+        porta, eu ornare velit dictum. Curabitur vitae augue et justo bibendum
+        ornare. Cras vitae lectus sed ligula blandit placerat. Nulla aliquet
+        sodales metus, sit amet ultricies ligula pulvinar sed. Proin sed mauris
+        ac nisl consectetur hendrerit at eu nibh.
+      </div>
     </main>
   )
 }
