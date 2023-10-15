af5257f6e1dfc270f90d8aa22528576c776bd8b2
Thigo <thi.it.solutions@gmail.com>
2023-10-15 18:38:51 -0300

    fix(): deploy error

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,19 @@
 /** @type {import('next').NextConfig} */
+
+const path = require('path')
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['cdn.sanity.io'],
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, './'),
+    }
+
+    return config
   },
 }
 
