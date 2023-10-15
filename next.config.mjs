364c45dd6cb6d5c11140cff58a59a78bc85f7cb3
Thigo <thi.it.solutions@gmail.com>
2023-10-15 19:22:51 -0300

    deploy(): fixing all error adding new auth method

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 import path from 'path'
-import { fileURLToPath } from 'node:url'
 
-const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 const nextConfig = {
